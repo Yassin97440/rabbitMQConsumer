5059f8fc8d3aa8df3c628f013f4b02ee00ca0d45
Yassin <yassin.abdulla@compufirst.com>
2022-10-24 15:04:15 +0200

    first commit
    Signed-off-by: Yassin <yassin.abdulla@compufirst.com>

---
+++ src/test/java/com/compufirst/rabbitMQ/consumer/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compufirst.rabbitMQ.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
